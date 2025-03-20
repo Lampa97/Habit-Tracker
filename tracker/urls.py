89d7fd3b06ecbf0929712bcc30758304bb720e89
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-03-20 11:19:50 +0300

    added views for Habit model

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import HabitListAPIView, HabitCreateAPIView, HabitDeleteAPIView, HabitDetailAPIView, HabitUpdateAPIView, PublicHabitListAPIView
 
 from tracker.apps import TrackerConfig
 
@@ -6,5 +7,10 @@ app_name = TrackerConfig.name
 
 
 urlpatterns = [
-
+    path("habits/", HabitListAPIView.as_view(), name="habit-list"),
+    path("habits/public/", PublicHabitListAPIView.as_view(), name="public-habit-list"),
+    path("habits/create/", HabitCreateAPIView.as_view(), name="habit-create"),
+    path("habits/<int:pk>/", HabitDetailAPIView.as_view(), name="habit-detail"),
+    path("habits/<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habit-update"),
+    path("habits/<int:pk>/delete/", HabitDeleteAPIView.as_view(), name="habit-delete"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 
-# Create your views here.
+from .models import Habit
+from .serializers import HabitSerializer
+from .paginators import CustomPagination
+
+class HabitCreateAPIView(CreateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class PublicHabitListAPIView(ListAPIView):
+    queryset = Habit.objects.filter(is_public=True)
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
+
+
+class HabitDetailAPIView(RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDeleteAPIView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
